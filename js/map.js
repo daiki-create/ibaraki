bf6992bf0095289d84c23367c7434cae292ffe7c
daiki <6280ikiad@gmail.com>
2022-04-07 23:54:27 +0900

    PCマップ以外完成

---
+++ js/map.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+function initialize() {
+    var matue = new google.maps.LatLng(36.487523, 140.401683);
+    var mapOptions = {
+        zoom: 9,
+        center: matue
+    }
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var ctaLayer = new google.maps.KmlLayer({
+        url: 'kml/shimane.kml'
+    });
+    ctaLayer.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
