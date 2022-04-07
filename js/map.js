f7a59a848ae830af720eb601e65a493afc6d0af5
daiki <6280ikiad@gmail.com>
2022-04-08 01:45:36 +0900

    kml -m

--- js/map.js
+++ js/map.js
@@ -14,11 +14,12 @@
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     var ctaLayer = new google.maps.KmlLayer({
+        url: 'http://localhost/ibaraki/kml/ibaraki_2.kml',
         // url: 'http://noland.sakura.ne.jp/ibaraki/kml/ibaraki.kml',
-        
+
         // url: 'https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml',
         // url: 'http://localhost/ibaraki/kml/test.kml',
-        url: 'http://noland.sakura.ne.jp/ibaraki/kml/test.kml',
+        // url: 'http://noland.sakura.ne.jp/ibaraki/kml/test.kml',
     });
     ctaLayer.setMap(map);
 // }
