3dc3c73065aab1332d5c9fb0b65dee3659d32cf9
daiki <6280ikiad@gmail.com>
2022-04-08 00:45:13 +0900

    kml -m

--- js/map.js
+++ js/map.js
@@ -5,18 +5,19 @@
 
 
 
-function initialize() {
+// function initialize() {
     var matue = new google.maps.LatLng(36.487523, 140.401683);
     var mapOptions = {
         zoom: 9,
         center: matue
     }
-
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
     var ctaLayer = new google.maps.KmlLayer({
-        url: 'kml/shimane.kml'
+        url: 'http://noland.sakura.ne.jp/ibaraki/kml/ibaraki.kml',
+        map: map
     });
     ctaLayer.setMap(map);
-}
+// }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', initialize);
